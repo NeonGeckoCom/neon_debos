6b2739af7e1f3ba62884c0b2d68bf7f5ec44f380
Aditya Mehra <aix.m@outlook.com>
2021-03-03 16:41:34 +0530

    fix scripts for correct dir

--- scripts/92-clone-mycroft.sh
+++ scripts/92-clone-mycroft.sh
@@ -5,22 +5,21 @@ dpkg --install /var/tmp/mimic.deb
 
 # Unpack mycroft-core-setup-aarch64.tar.gz to user home
 git clone https://github.com/MycroftAI/mycroft-core ./home/$USER/
-cd /home/$USER/mycroft-core
 
 # Get latest patches
-wget https://raw.githubusercontent.com/OpenVoiceOS/OpenVoiceOS/develop/buildroot-external/package/python-mycroft/0001-Bump-requests-requirement-to-2.24.0-inline-with-buil.patch
-wget https://raw.githubusercontent.com/OpenVoiceOS/OpenVoiceOS/develop/buildroot-external/package/python-mycroft/0002-Shut-up.patch
-wget https://raw.githubusercontent.com/OpenVoiceOS/OpenVoiceOS/develop/buildroot-external/package/python-mycroft/0003-OVOS-PR2843.patch
-wget https://raw.githubusercontent.com/OpenVoiceOS/OpenVoiceOS/develop/buildroot-external/package/python-mycroft/0004-OVOS-enclosure_assumptions.patch 
+(cd /home/$USER/mycroft-core && wget https://raw.githubusercontent.com/OpenVoiceOS/OpenVoiceOS/develop/buildroot-external/package/python-mycroft/0001-Bump-requests-requirement-to-2.24.0-inline-with-buil.patch)
+(cd /home/$USER/mycroft-core && wget https://raw.githubusercontent.com/OpenVoiceOS/OpenVoiceOS/develop/buildroot-external/package/python-mycroft/0002-Shut-up.patch)
+(cd /home/$USER/mycroft-core && wget https://raw.githubusercontent.com/OpenVoiceOS/OpenVoiceOS/develop/buildroot-external/package/python-mycroft/0003-OVOS-PR2843.patch)
+(cd /home/$USER/mycroft-core && wget https://raw.githubusercontent.com/OpenVoiceOS/OpenVoiceOS/develop/buildroot-external/package/python-mycroft/0004-OVOS-enclosure_assumptions.patch)
 
 # Apply all patches
-git am 0001-Bump-requests-requirement-to-2.24.0-inline-with-buil.patch
-git am 0002-Shut-up.patch
-git am 0003-OVOS-PR2843.patch
-git am 0004-OVOS-enclosure_assumptions.patch
+(cd /home/$USER/mycroft-core && git am 0001-Bump-requests-requirement-to-2.24.0-inline-with-buil.patch)
+(cd /home/$USER/mycroft-core && git am 0002-Shut-up.patch)
+(cd /home/$USER/mycroft-core && git am 0003-OVOS-PR2843.patch)
+(cd /home/$USER/mycroft-core && git am 0004-OVOS-enclosure_assumptions.patch)
 
 # Install mycroft core
-bash dev_setup.sh
+(cd /home/$USER/mycroft-core && bash dev_setup.sh)
 
 # Add Mycroft helper commands to $PATH
 echo 'source /opt/mycroft/.mycroftrc' >> /home/$USER/.bashrc
--- scripts/97-wifi-connect.sh
+++ scripts/97-wifi-connect.sh
@@ -2,6 +2,4 @@ UGID=32011
 USER=mycroft
 
 tar -xzf /var/tmp/wifi-connect-aarch64.tar.gz -C /home/$USER/
-cd /home/$USER/
-wget https://github.com/balena-io/wifi-connect/raw/master/scripts/raspbian-install.sh
-bash raspbian-install.sh
+(cd /home/$USER/wifi-connect-aarch64 && bash raspbian-install.sh)
