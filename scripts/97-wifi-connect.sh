e50c0214ff6b6c81e6d71023037eaf7338a2f27f
Aditya Mehra <aix.m@outlook.com>
2021-03-03 14:58:56 +0530

    simplify wifi connect script

--- scripts/97-wifi-connect.sh
+++ scripts/97-wifi-connect.sh
@@ -3,4 +3,5 @@ USER=mycroft
 
 tar -xzf /var/tmp/wifi-connect-aarch64.tar.gz -C /home/$USER/
 cd /home/$USER/
-bash <(curl -L https://github.com/balena-io/wifi-connect/raw/master/scripts/raspbian-install.sh)
+wget https://github.com/balena-io/wifi-connect/raw/master/scripts/raspbian-install.sh
+bash raspbian-install.sh
