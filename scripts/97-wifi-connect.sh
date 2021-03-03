f140812ae7c3ebd6594478827d9e80a853abdb72
Aditya Mehra <aix.m@outlook.com>
2021-03-03 14:23:07 +0530

    fix wifi connect script

--- scripts/97-wifi-connect.sh
+++ scripts/97-wifi-connect.sh
@@ -1,6 +1,6 @@
 UGID=32011
 USER=mycroft
 
-tar -xzf /var/tmp/wifi-connect.tar.gz -C /home/$USER/
+tar -xzf /var/tmp/wifi-connect-aarch64.tar.gz -C /home/$USER/
 cd /home/$USER/
 bash <(curl -L https://github.com/balena-io/wifi-connect/raw/master/scripts/raspbian-install.sh)
