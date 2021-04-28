7a3c5c0c0a8d9b5052c8f3ace5079febb7675054
Aditya Mehra <aix.m@outlook.com>
2021-04-29 04:08:41 +0530

    fix pip script and missing dep

--- scripts/92-pip-mycroft.sh
+++ scripts/92-pip-mycroft.sh
@@ -1,22 +1,18 @@
-UGID=32011
-USER=mycroft
-
 # Unpack mycroft-core to user home
-git clone https://github.com/HelloChatterbox/mycroft-lib/ ./home/$USER/
+git clone https://github.com/HelloChatterbox/mycroft-lib
 
 # Get latest patches
-(cd /home/$USER/mycroft-lib && wget https://github.com/OpenVoiceOS/OpenVoiceOS/blob/develop/buildroot-external/package/python-mycroft-lib/0001-Add-entry_points-to-install-binaries.patch)
+wget https://github.com/OpenVoiceOS/OpenVoiceOS/blob/develop/buildroot-external/package/python-mycroft-lib/0001-Add-entry_points-to-install-binaries.patch
 
 # Apply all patches
-(cd /home/$USER/mycroft-lib && git am 0001-Add-entry_points-to-install-binaries.patch)
+git am 0001-Add-entry_points-to-install-binaries.patch
 
 # Install mycroft core
-pip install .[all]
+pip3 install .[all]
 
 # Add Mycroft helper commands to $PATH
 # echo 'source /opt/mycroft/.mycroftrc' >> /home/$USER/.bashrc
 
 mkdir -p /var/log/mycroft
-
 chown -R $UGID:$UGID /home/$USER
 chown -R $UGID:$UGID /var/log/mycroft
