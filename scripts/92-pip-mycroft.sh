212b702ce352a82c069fabb79b353a5bdae7dab2
Aditya Mehra <aix.m@outlook.com>
2021-04-29 12:46:32 +0530

    pip script fix try tmp

--- scripts/92-pip-mycroft.sh
+++ scripts/92-pip-mycroft.sh
@@ -1,18 +1,18 @@
 # Unpack mycroft-core to user home
-git clone https://github.com/HelloChatterbox/mycroft-lib
+git clone https://github.com/HelloChatterbox/mycroft-lib ./tmp/mycroft-lib
 
 # Get latest patches
-cd mycroft-lib && wget https://github.com/OpenVoiceOS/OpenVoiceOS/blob/develop/buildroot-external/package/python-mycroft-lib/0001-Add-entry_points-to-install-binaries.patch
+(cd /tmp/mycroft-lib && wget https://github.com/OpenVoiceOS/OpenVoiceOS/blob/develop/buildroot-external/package/python-mycroft-lib/0001-Add-entry_points-to-install-binaries.patch)
 
 # Apply all patches
-cd mycroft-lib && git am 0001-Add-entry_points-to-install-binaries.patch
+(cd /tmp/mycroft-lib && git am 0001-Add-entry_points-to-install-binaries.patch)
 
 # Install mycroft core
-cd mycroft-lib && pip3 install .[all]
+(cd /tmp/mycroft-lib && pip3 install .[all])
 
 # Add Mycroft helper commands to $PATH
 # echo 'source /opt/mycroft/.mycroftrc' >> /home/$USER/.bashrc
 
 mkdir -p /var/log/mycroft
-chown -R $UGID:$UGID /home/$USER
-chown -R $UGID:$UGID /var/log/mycroft
+chown -R 32011:32011 /home/mycroft
+chown -R 32011:32011 /var/log/mycroft
