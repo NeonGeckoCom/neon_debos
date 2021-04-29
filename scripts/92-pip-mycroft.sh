a61cccc899a6c8d17e59d52fba2a203fd8885730
Aditya Mehra <aix.m@outlook.com>
2021-04-29 11:09:29 +0530

    add missing service files and fix pip mycroft lib install

--- scripts/92-pip-mycroft.sh
+++ scripts/92-pip-mycroft.sh
@@ -2,13 +2,13 @@
 git clone https://github.com/HelloChatterbox/mycroft-lib
 
 # Get latest patches
-wget https://github.com/OpenVoiceOS/OpenVoiceOS/blob/develop/buildroot-external/package/python-mycroft-lib/0001-Add-entry_points-to-install-binaries.patch
+cd mycroft-lib && wget https://github.com/OpenVoiceOS/OpenVoiceOS/blob/develop/buildroot-external/package/python-mycroft-lib/0001-Add-entry_points-to-install-binaries.patch
 
 # Apply all patches
-git am 0001-Add-entry_points-to-install-binaries.patch
+cd mycroft-lib && git am 0001-Add-entry_points-to-install-binaries.patch
 
 # Install mycroft core
-pip3 install .[all]
+cd mycroft-lib && pip3 install .[all]
 
 # Add Mycroft helper commands to $PATH
 # echo 'source /opt/mycroft/.mycroftrc' >> /home/$USER/.bashrc
