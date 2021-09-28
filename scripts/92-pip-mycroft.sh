a893d9a1a420e25638fc37911a3cc1ed0e77655e
Aditya Mehra <aix.m@outlook.com>
2021-09-28 16:59:50 +0930

    Add ci instructions and fix scripts

--- scripts/92-pip-mycroft.sh
+++ scripts/92-pip-mycroft.sh
@@ -1,19 +1,21 @@
 # Unpack mycroft-core to user home
-git clone https://github.com/HelloChatterbox/mycroft-lib ./tmp/mycroft-lib
-
+# git clone https://github.com/HelloChatterbox/mycroft-lib ./tmp/mycroft-lib
+#
 # Get latest patches
-(cd /tmp/mycroft-lib && wget https://raw.githubusercontent.com/OpenVoiceOS/OpenVoiceOS/develop/buildroot-external/package/python-mycroft-lib/0001-Add-entry_points-to-install-binaries.patch)
-
+# (cd /tmp/mycroft-lib && wget https://raw.githubusercontent.com/OpenVoiceOS/OpenVoiceOS/develop/buildroot-external/package/python-mycroft-lib/0001-Add-entry_points-to-install-binaries.patch)
+#
 # Apply all patches
-(cd /tmp/mycroft-lib && git config user.name "aix")
-(cd /tmp/mycroft-lib && git config user.email "aix.m@outlook.com")
-(cd /tmp/mycroft-lib && git am 0001-Add-entry_points-to-install-binaries.patch)
-
+# (cd /tmp/mycroft-lib && git config user.name "aix")
+# (cd /tmp/mycroft-lib && git config user.email "aix.m@outlook.com")
+# (cd /tmp/mycroft-lib && git am 0001-Add-entry_points-to-install-binaries.patch)
+#
 # Install mycroft core
-(cd /tmp/mycroft-lib && pip3 install .[all])
+# (cd /tmp/mycroft-lib && pip3 install .[all])
 
 # Add Mycroft helper commands to $PATH
 # echo 'source /opt/mycroft/.mycroftrc' >> /home/$USER/.bashrc
+
+(pip3 install git+https://github.com/HelloChatterbox/HolmesV)
 
 mkdir -p /var/log/mycroft
 chown -R 32011:32011 /home/mycroft
--- scripts/99-install-ovos-skills.sh
+++ scripts/99-install-ovos-skills.sh
@@ -1,13 +1,19 @@
 # Install OVOS Skills
 mkdir -p /home/mycroft/.local/share/mycroft/skills/
 
-(cd /home/mycroft/.local/share/mycroft/skills && git clone https://github.com/OpenVoiceOS/skill-ovos-pairing)
-(cd /home/mycroft/.local/share/mycroft/skills/skill-ovos-pairing && pip3 install -r requirements.txt)
-(cd /home/mycroft/.local/share/mycroft/skills && git clone https://github.com/OpenVoiceOS/skill-ovos-mycroftgui)
-(cd /home/mycroft/.local/share/mycroft/skills/skill-ovos-mycroftgui && pip3 install -r requirements.txt)
-(cd /home/mycroft/.local/share/mycroft/skills && git clone https://github.com/OpenVoiceOS/skill-ovos-homescreen)
-(cd /home/mycroft/.local/share/mycroft/skills/skill-ovos-homescreen && pip3 install -r requirements.txt)
-(cd /home/mycroft/.local/share/mycroft/skills && git clone https://github.com/OpenVoiceOS/skill-balena-wifi-setup)
-(cd /home/mycroft/.local/share/mycroft/skills/skill-balena-wifi-setup && pip3 install -r requirements.txt)
+(cd /home/mycroft/.local/share/mycroft/skills && git clone https://github.com/OpenVoiceOS/skill-ovos-setup skill-ovos-setup.openvoiceos)
+(cd /home/mycroft/.local/share/mycroft/skills/skill-ovos-pairing.openvoiceos && pip3 install -r requirements.txt)
+(cd /home/mycroft/.local/share/mycroft/skills && git clone https://github.com/OpenVoiceOS/skill-ovos-mycroftgui skill-ovos-mycroftgui.openvoiceos)
+(cd /home/mycroft/.local/share/mycroft/skills/skill-ovos-mycroftgui.openvoiceos && pip3 install -r requirements.txt)
+(cd /home/mycroft/.local/share/mycroft/skills && git clone https://github.com/OpenVoiceOS/skill-ovos-homescreen skill-ovos-homescreen.openvoiceos)
+(cd /home/mycroft/.local/share/mycroft/skills/skill-ovos-homescreen.openvoiceos && pip3 install -r requirements.txt)
+(cd /home/mycroft/.local/share/mycroft/skills && git clone https://github.com/OpenVoiceOS/skill-balena-wifi-setup skill-balena-wifi-setup.openvoiceos)
+(cd /home/mycroft/.local/share/mycroft/skills/skill-balena-wifi-setup.openvoiceos && pip3 install -r requirements.txt)
+(cd /home/mycroft/.local/share/mycroft/skills && git clone https://github.com/OpenVoiceOS/skill-weather skill-weather.openvoiceos)
+(cd /home/mycroft/.local/share/mycroft/skills/skill-weather.openvoiceos && pip3 install -r requirements.txt)
+(cd /home/mycroft/.local/share/mycroft/skills && git clone https://github.com/MycroftAI/skill-date-time skill-date-time.mycroftai)
+(cd /home/mycroft/.local/share/mycroft/skills/skill-date-time.mycroftai && pip3 install -r requirements.txt)
+(cd /home/mycroft/.local/share/mycroft/skills && git clone https://github.com/OpenVoiceOS/ovos-skills-info ovos-skills-info.openvoiceos)
+(cd /home/mycroft/.local/share/mycroft/skills/ovos-skills-info.openvoiceos && pip3 install -r requirements.txt)
 
 chown -R 32011:32011 /home/mycroft/.local/share/mycroft/skills/
