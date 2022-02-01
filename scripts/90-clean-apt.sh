7beadadaee0eada516f278c3b7d0fef6ca99f733
Aditya Mehra <aix.m@outlook.com>
2022-02-02 02:10:37 +1030

    try fix up cleanup script

--- scripts/90-clean-apt.sh
+++ scripts/90-clean-apt.sh
@@ -1,7 +1,3 @@
 # Clean Up Apt Packages
 (echo "**** Clean Up All Apt Cache ****")
-(cd /var/cache/apt/ && rm -rf archives/*)
-
-# Clean Up Apt Packages
-(echo "**** Clean Up All Pypi Cache ****")
-(sudo pip3 cache purge)
+rm -rf /var/cache/apt/archives/*
