03b3c64a8f464222db5ec8f2eedb99ee32112007
Daniel McKnight <daniel@neon.ai>
2022-09-27 20:07:58 -0700

    Refactor recipes for readability WIP wifi-connect fixes

--- run_docker_debos.sh
+++ run_docker_debos.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-
+source_dir="/home/$USER/neon_debos"
 docker run --rm \
 --device /dev/kvm \
 --workdir /image_build \
---mount type=bind,source=/home/$USER/PycharmProjects/neon_debos,destination=/image_build \
+--mount type=bind,source="${source_dir}",destination=/image_build \
 --group-add=108 \
 --security-opt label=disable \
 godebos/debos debian-base-image-rpi4.yml -t architecture:arm64 -m 10G -c 6
+
+#sudo chown $USER:$USER "${source_dir}"/*
