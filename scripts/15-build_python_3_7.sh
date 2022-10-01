70843140cd17954af60ae7f78f4bf52666abec38
Daniel McKnight <daniel@neon.ai>
2022-09-30 17:27:47 -0700

    Update skel files for added users Update udev rules Add gui dependency installation with patches Add splash screen build step Update Python 3.7 build

--- scripts/15-build_python_3_7.sh
+++ scripts/15-build_python_3_7.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Set to exit on error
-#set -Ee
+set -Ee
 
 cd /opt || exit 10
 wget https://www.python.org/ftp/python/3.7.14/Python-3.7.14.tar.xz
 tar -xf Python-3.7.14.tar.xz
+rm Python-3.7.14.tar.xz
 cd Python-3.7.14 || exit 10
-./configure --enable-optimizations --enable-shared && echo "Configure complete"
-echo ">>>configure finished"
-make -j4 && echo "make complete"
+./configure --enable-optimizations && echo "Configure complete"
+make -j2 && echo "make complete"
 echo ">>>make finished"
 make altinstall && echo "install complete"
 echo ">>>altinstall finished"
 ldconfig /opt/Python-3.7.14 && echo "python-3.7 libs linked"
-# TODO: Can /opt/Python-3.7.* be removed?
+rm -rf /opt/Python-3.7.14
