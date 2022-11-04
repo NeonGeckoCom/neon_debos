537613125ff69f29f45c3e490ba1c83c6075c30a
Daniel McKnight <daniel@neon.ai>
2022-11-04 15:38:48 -0700

    Replace keyboard build with deb package install

--- scripts/12-build-qtvirtualkeyboard.sh
+++ scripts/12-build-qtvirtualkeyboard.sh
@@ -3,14 +3,4 @@
 # Set to exit on error
 set -Ee
 
-git clone https://invent.kde.org/qt/qt/qtvirtualkeyboard/ -b v5.15.4-lts-lgpl
-cd qtvirtualkeyboard || exit 10
-mv /opt/qvirtualkeyboardinputcontext_p.cpp src/virtualkeyboard/
-
-mkdir build
-cd build || exit 10
-qmake .. CONFIG+="lang-all handwriting"
-make -j4
-make install
-cd ../..
-rm -rf qtvirtualkeyboard
+dpkg --force-overwrite -i /opt/qtvirtualkeyboard_5.15.6_arm64.deb
