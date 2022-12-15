fbf92871599cdf9cbb6a0974c916a9edb53d9138
Daniel McKnight <daniel@neongecko.com>
2022-12-15 15:28:38 -0800

    Fix up kernel handling to use built 5.15 Kernel Add userland installation from built .deb packages

--- scripts/02-patch_5_15_raspi_kernel.sh
+++ scripts/02-patch_5_15_raspi_kernel.sh
@@ -27,17 +27,22 @@
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE,  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-# Get built RPi kernels from raspberrypi archives
-
 # Set to exit on error
 set -Ee
 
-kernel_version="1.20220308~buster-1"
+export HEADER_DIR=/usr/src/linux-headers-5.15.72-v8+
 
-wget http://archive.raspberrypi.org/debian/pool/main/r/raspberrypi-firmware/raspberrypi-kernel_${kernel_version}_arm64.deb
-wget http://archive.raspberrypi.org/debian/pool/main/r/raspberrypi-firmware/raspberrypi-kernel-headers_${kernel_version}_arm64.deb
-wget http://archive.raspberrypi.org/debian/pool/main/r/raspberrypi-firmware/linux-libc-dev_${kernel_version}_arm64.deb
-wget http://archive.raspberrypi.org/debian/pool/main/r/raspberrypi-firmware/raspberrypi-bootloader_${kernel_version}_arm64.deb
-dpkg -i ./*.deb
-rm ./*.deb
-echo "Kernel installation complete"
+find "$HEADER_DIR/scripts" -type f | while read i; do if file -b $i | egrep -q "^ELF.*x86-64"; then rm "$i"; fi; done
+cd $HEADER_DIR/scripts || exit 2
+gcc kallsyms.c -o "$HEADER_DIR/scripts/kallsyms"
+#gcc pnmtologo.c -o "$HEADER_DIR/scripts/pnmtologo"
+#gcc conmakehash.c -o "$HEADER_DIR/scripts/conmakehash"
+gcc recordmcount.c -o "$HEADER_DIR/scripts/recordmcount"
+#gcc -I../tools/include sortextable.c -o "$HEADER_DIR/scripts/sortextable"
+gcc unifdef.c -o "$HEADER_DIR/scripts/unifdef"
+gcc ./basic/fixdep.c -o "$HEADER_DIR/scripts/basic/fixdep"
+gcc extract-cert.c -o "$HEADER_DIR/scripts/extract-cert" -lssl -lcrypto
+gcc ./mod/modpost.c ./mod/file2alias.c ./mod/sumversion.c -o "$HEADER_DIR/scripts/mod/modpost"
+gcc ./mod/mk_elfconfig.c -o "$HEADER_DIR/scripts/mod/mk_elfconfig"
+gcc  -I../include asn1_compiler.c -o "$HEADER_DIR/scripts/asn1_compiler"
+gcc ./genksyms/genksyms.c ./genksyms/parse.tab.c ./genksyms/lex.lex.c -o "$HEADER_DIR/scripts/genksyms/genksyms"
