8e278ec5d2113309c29a871b7f5f6d3711f6836f
Daniel McKnight <daniel@neongecko.com>
2022-12-20 10:02:01 -0800

    Cleanup kernel patching

--- scripts/02-patch_5_15_raspi_kernel.sh
+++ scripts/02-patch_5_15_raspi_kernel.sh
@@ -35,10 +35,7 @@ export HEADER_DIR=/usr/src/linux-headers-5.15.72-v8+
 find "$HEADER_DIR/scripts" -type f | while read i; do if file -b $i | egrep -q "^ELF.*x86-64"; then rm "$i"; fi; done
 cd $HEADER_DIR/scripts || exit 2
 gcc kallsyms.c -o "$HEADER_DIR/scripts/kallsyms"
-#gcc pnmtologo.c -o "$HEADER_DIR/scripts/pnmtologo"
-#gcc conmakehash.c -o "$HEADER_DIR/scripts/conmakehash"
 gcc recordmcount.c -o "$HEADER_DIR/scripts/recordmcount"
-#gcc -I../tools/include sortextable.c -o "$HEADER_DIR/scripts/sortextable"
 gcc unifdef.c -o "$HEADER_DIR/scripts/unifdef"
 gcc ./basic/fixdep.c -o "$HEADER_DIR/scripts/basic/fixdep"
 gcc extract-cert.c -o "$HEADER_DIR/scripts/extract-cert" -lssl -lcrypto
