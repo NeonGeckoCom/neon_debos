4f69b2086a7a10f3354a4c24b45b4c8431f0218a
Daniel McKnight <daniel@neongecko.com>
2023-03-28 13:26:14 -0700

    Update swap partition creation to suppress warnings requiring input

--- overlays/99-squashfs/opt/neon/prepare_fs.sh
+++ overlays/99-squashfs/opt/neon/prepare_fs.sh
@@ -5,7 +5,7 @@ root_dev="sda"
 total_size=$(lsblk -b | grep "^${root_dev}" | awk '{print $4}' | cut -dG -f1)
 total_kB=$((total_size / 1024))
 start_kB=$((total_kB - 1048576))
-parted -a minimal /dev/sda mkpart primary linux-swap "${start_kB}kB" "${total_kB}kB"
+parted -a optimal /dev/sda mkpart primary linux-swap "${start_kB}kiB" "${total_kB}kiB"
 mkswap /dev/${root_dev}4
 
 growpart /dev/${root_dev} 3
