beb75f034e5de5a742d653eb50ec4a035c807ed1
Daniel McKnight <daniel@neongecko.com>
2023-03-29 16:26:20 -0700

    Fix first boot partition resizing Add extra FS Overlay to Neon image recipe

--- overlays/99-squashfs/opt/neon/prepare_fs.sh
+++ overlays/99-squashfs/opt/neon/prepare_fs.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+set -Ee
+
 root_dev="sda"
 
 total_size=$(lsblk -b | grep "^${root_dev}" | awk '{print $4}' | cut -dG -f1)
-total_kB=$((total_size / 1024))
-start_kB=$((total_kB - 1048576))
-parted -a optimal /dev/sda mkpart primary linux-swap "${start_kB}kiB" "${total_kB}kiB"
+total_MiB=$((total_size / 1048576))
+start_MiB=$((total_MiB - 1024))
+echo "total_size=${total_size} total_MiB=${total_MiB} start_MiB=${start_MiB}"
+parted -a optimal /dev/sda mkpart primary linux-swap "${start_MiB}MiB" "${total_MiB}MiB"
 mkswap /dev/${root_dev}4
 
+#echo "yes" | parted -a optimal /dev/sda ---pretend-input-tty resizepart 3 "${start_MiB}MiB"
 growpart /dev/${root_dev} 3
 resize2fs /dev/sda3
