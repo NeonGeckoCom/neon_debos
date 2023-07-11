86c3a8e6785375218009443bdc9759b5502a9aea
Daniel McKnight <daniel@neongecko.com>
2023-07-11 12:55:22 -0700

    Leave up to 1MiB space at end to prevent partition errors

--- overlays/99-squashfs/opt/neon/prepare_fs.sh
+++ overlays/99-squashfs/opt/neon/prepare_fs.sh
@@ -5,7 +5,7 @@ set -Ee
 root_dev="sda"
 
 total_size=$(lsblk -b | grep "^${root_dev}" | awk '{print $4}' | cut -dG -f1)
-total_MiB=$((total_size / 1048576))
+total_MiB=$(($((total_size / 1048576)) - 1))
 start_MiB=$((total_MiB - 1024))
 echo "total_size=${total_size} total_MiB=${total_MiB} start_MiB=${start_MiB}"
 parted -a optimal /dev/sda mkpart primary linux-swap "${start_MiB}MiB" "${total_MiB}MiB"
