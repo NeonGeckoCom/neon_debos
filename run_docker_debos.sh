e2f30ec155cf7b07e3d752b28fc6a7f22c63fe62
Daniel McKnight <daniel@neongecko.com>
2023-07-24 14:08:42 -0700

    Update `image` variable handling to better manage paths and file extensions

--- run_docker_debos.sh
+++ run_docker_debos.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 source_dir="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+mkdir "${source_dir}/output"
 timestamp=$(date '+%Y-%m-%d_%H_%M')
 image=${1:-"debian-neon-image-rpi4.yml"}
 neon_core=${2:-"master"}
@@ -11,7 +12,7 @@ docker run --rm -d \
 --group-add=108 \
 --security-opt label=disable \
 --name neon_debos \
-godebos/debos "${image}" -t architecture:arm64 -t image:"${image}_${timestamp}.img" -t neon_core:"${neon_core}" -m 24G -c 4 && \
+godebos/debos "${image}" -t architecture:arm64 -t image:"${image%.*}_${timestamp}" -t neon_core:"${neon_core}" -m 24G -c 4 && \
 docker logs -f neon_debos
 
 #sudo chown $USER:$USER "${source_dir}"/*
