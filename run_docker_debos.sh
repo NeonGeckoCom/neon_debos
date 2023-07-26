3eddf684cbb68a71972c6f0f8865b8f21aa437b9
Daniel McKnight <daniel@neongecko.com>
2023-07-25 21:53:21 -0700

    Finalize image with squashfs Update script to fix ownership of generated images

--- run_docker_debos.sh
+++ run_docker_debos.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source_dir="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-mkdir "${source_dir}/output"
+[ -d "${source_dir}/output" ] || mkdir "${source_dir}/output"
 timestamp=$(date '+%Y-%m-%d_%H_%M')
 image=${1:-"debian-neon-image-rpi4.yml"}
 neon_core=${2:-"master"}
@@ -14,5 +14,5 @@ docker run --rm -d \
 --name neon_debos \
 godebos/debos "${image}" -t architecture:arm64 -t image:"${image%.*}_${timestamp}" -t neon_core:"${neon_core}" -m 24G -c 4 && \
 docker logs -f neon_debos
-
-#sudo chown $USER:$USER "${source_dir}"/*
+echo "completed ${timestamp}"
+sudo chown $USER:$USER "${source_dir}/output/${image%.*}_${timestamp}"*
