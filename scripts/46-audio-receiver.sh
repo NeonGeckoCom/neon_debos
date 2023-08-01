57d2bea39cd145bdddef416cd051b0f7ec362af2
Mike Gray <mike@graywind.org>
2023-07-31 20:48:58 -0500

    switch order of bluetooth usermod

--- scripts/46-audio-receiver.sh
+++ scripts/46-audio-receiver.sh
@@ -1,4 +1,5 @@
 sed -i '/LIBRESPOT_NAME/c\LIBRESPOT_NAME="Neon Mark 2 - '"$(hostname)"'"' /etc/raspotify/conf
+usermod -aG bluetooth pulse
 pactl load-module module-bluetooth-discover
 pactl load-module module-bluetooth-policy
 systemctl enable uxplay.service
