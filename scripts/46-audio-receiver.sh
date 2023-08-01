f55034384d6f1ca464da32b001be2443e436a1ed
Mike Gray <mike@graywind.org>
2023-07-31 23:22:41 -0500

    fix raspotify installation

--- scripts/46-audio-receiver.sh
+++ scripts/46-audio-receiver.sh
@@ -1,8 +1,10 @@
+sudo apt-get -y install curl && curl -sL https://dtcooper.github.io/raspotify/install.sh | sh
 sed -i '/LIBRESPOT_NAME/c\LIBRESPOT_NAME="Neon Mark 2 - '"$(hostname)"'"' /etc/raspotify/conf
 usermod -aG bluetooth pulse
 pactl load-module module-bluetooth-discover
 pactl load-module module-bluetooth-policy
 systemctl enable uxplay.service
+systemctl enable raspotify.service
 systemctl enable kdeconnect.service
 systemctl enable bluetooth.service
 systemctl enable bluetooth-agent
