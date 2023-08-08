24de1691c6c928ecf326a5745c4d5383bd70ba62
Daniel McKnight <daniel@neongecko.com>
2023-08-08 09:09:58 -0700

    Update automation to handle output file relocation Comment out build steps to test automation

--- run_automation.sh
+++ run_automation.sh
@@ -6,8 +6,9 @@
 source_dir="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 [ -d "${source_dir}/output" ] || mkdir "${source_dir}/output"
 timestamp=$(date '+%Y-%m-%d_%H_%M')
-image=${1:-"debian-neon-image-rpi4.yml"}
-neon_core=${2:-"master"}
+image=${1:?}
+neon_core=${2:?}
+output_dir=${3:?}
 # TODO: Configurable runner limits
 mem_limit=${MEM_LIMIT:-"64G"}
 core_limit=${CORE_LIMIT:-32}
@@ -20,4 +21,6 @@ docker run --rm \
 --security-opt label=disable \
 --name neon_debos_ghaction \
 godebos/debos "${image}" -t architecture:arm64 -t image:"${image%.*}_${timestamp}" -t neon_core:"${neon_core}" -t build_cores:"${core_limit}" -m "${mem_limit}" -c "${core_limit}" || exit 2
+mv "${source_dir}/output/"*.img.xz "${output_dir}"
+mv "${source_dir}/output/"*.squashfs "${output_dir}"
 echo "completed ${timestamp}"
