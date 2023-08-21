94089e623a201962b1d6bdaa507e99d6b524b312
Mike <mike@graywind.org>
2023-08-21 12:50:34 -0500

    feat: Mark 2 Audio Receiver (#23)
    * feat: install raspotify by default
    
    * feat: UxPlay for AirPlay services
    
    * update Librespot name to include the hostname of the assistant
    
    * feat: kdeconnect
    
    There are no media players available by default when running KDEConnect.
    
    Not sure why OCP doesn't show up, but VLC does if you open it from the CLI.
    
    * checking in
    
    Not quite right yet. It's not advertising properly.
    
    * feat: bluetooth draft
    
    * feat: bluetooth base support
    
    * switch order of bluetooth usermod
    
    * fix raspotify installation
    
    * remove unused service
    
    * fix: changes from testing
    
    * fix: save script to file
    
    * further debugging
    
    * next steps
    
    * fix: issues with bluetooth service file
    
    * fix: configuration for KDE Connect control of OCP
    
    * updated files from local testing
    
    * accept timeout parameter
    
    * requested changes
    
    * add new environment vars
    
    * fixes from testing
    
    * proper use of arrays
    
    * more fixes
    
    * final corrections to environment variables
    
    * fix permissions, install skills
    
    * Update `pip` call in audio-receiver script
    
    * Remove breaking build step (group already exists)
    
    ---------
    
    Co-authored-by: Daniel McKnight <daniel@neongecko.com>
    Co-authored-by: Daniel McKnight <34697904+NeonDaniel@users.noreply.github.com>

---
+++ overlays/46-audio-receiver/usr/local/bin/autopair-kdeconnect.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+source /etc/neon/neon_env.conf
+
+# Set default timeout to 30 seconds
+timeout_duration=30
+
+# If an argument is provided, use it as the timeout
+if [ "$#" -eq 1 ]; then
+    timeout_duration="$1"
+    elif [ "$#" -gt 1 ]; then
+    echo "Usage: $0 [timeout_in_seconds]"
+    exit 1
+fi
+
+# Create a cleanup function
+cleanup() {
+    pkill -P $$ # Kill all subprocesses started by this script
+}
+
+trap cleanup EXIT
+
+# Use timeout to run dbus-monitor for the specified duration
+timeout "$timeout_duration" dbus-monitor --session "interface='org.freedesktop.Notifications',member='Notify'" |
+while read -r line; do
+    if [[ $line == *"Pairing request from"* ]]; then
+        # Extract the full device name
+        device_name=$(echo "$line" | grep -oP 'Pairing request from \K(.*?)(?= Key:)')
+        
+        echo "Extracted Device Name: $device_name"
+        
+        # Extract the device ID using the device name
+        device_id=$(kdeconnect-cli -l | grep -E "$device_name:" | awk -F': ' '{print $2}' | awk '{print $1}')
+        
+        # Pair with the device using the device ID
+        if [ ! -z "$device_id" ]; then
+            kdeconnect-cli -d "$device_id" --pair
+        else
+            echo "Couldn't find device ID for $device_name"
+        fi
+    fi
+done
---
+++ scripts/46-audio-receiver.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+export LIBRESPOT_NAME=${1:-"Neon Mark 2"}
+# Raspotify installation script
+apt-get -y install curl && curl -sL https://dtcooper.github.io/raspotify/install.sh -o install.sh
+## Sudo causes issues in build VM, so strip it out
+sed -i '/SUDO="sudo"/c\SUDO=""' install.sh
+sh install.sh
+# Adjust the service file to wait for the network to be online and for avahi-daemon to be running
+sed -i '/^Wants=network.target sound.target$/c\Wants=network-online.target sound.target avahi-daemon.service ntp.service' /lib/systemd/system/raspotify.service
+sed -i '/^After=network.target sound.target$/c\After=network-online.target sound.target avahi-daemon.service ntp.service' /lib/systemd/system/raspotify.service
+# Name the device for Spotify
+sed -i '/LIBRESPOT_NAME/c\LIBRESPOT_NAME="Neon Mark 2"' /etc/raspotify/conf
+# Set up Bluetooth audio permissions
+usermod -aG bluetooth pulse
+# Enable all services
+systemctl enable uxplay.service
+systemctl enable kdeconnect.service
+systemctl enable bluetooth.service
+systemctl enable raspotify.service
+# Fix permissions
+sudo chown -R neon:neon /home/neon/.cache/pip
+# Install packages for Bluetooth pairing script
+# TODO: Python packages to NeonCore dependencies(?)
+/home/neon/venv/bin/python -m pip install pydbus
+/home/neon/venv/bin/python -m pip install /home/neon/.cache/pip/wheels/5a/83/f3/aa5c753e241ee76cc586065f88f0e750c9bb370c181f1ab40a/PyBluez-0.23-cp310-cp310-linux_aarch64.whl
+/home/neon/venv/bin/python -m pip install /home/neon/.cache/pip/wheels/33/99/2c/061d0c6934509a9cc08470980d68fbfc972bd22a45c3f306d6/PyGObject-3.44.1-cp310-cp310-linux_aarch64.whl
+/home/neon/venv/bin/python -m pip install git+https://github.com/mikejgray/neon-phal-plugin-audio-receiver
+/home/neon/venv/bin/python -m pip install git+https://github.com/mikejgray/skill-mark2-audio-receiver/
