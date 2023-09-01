b3e1d3dca3de1b9e3a384dcea6770789c90a8de7
Daniel McKnight <daniel@neongecko.com>
2023-08-31 18:44:46 -0700

    Update scripts to log to dmesg Update dmesg to write to /var/log/initramfs.log to access logs after boot

--- initramfs_build/overlay/scripts/check_update.sh
+++ initramfs_build/overlay/scripts/check_update.sh
@@ -33,19 +33,21 @@ ROOT_FILE=${3}
 BACKUP_PATH=${4}
 BACKUP_FILE=${5:-""}
 
+. /scripts/functions
+
 # Validate update and optionally create backup
 if [ ! -f "${UPDATE_FILE}" ]; then
-  echo "No update (${UPDATE_FILE})"
+  log "No update (${UPDATE_FILE})"
   exit 0
 elif [ -n "${BACKUP_FILE}" ]; then
-  mv "${ROOT_FILE}" "${BACKUP_FILE}" && echo "Backed up existing image ${ROOT_FILE} to ${BACKUP_FILE}"
+  mv "${ROOT_FILE}" "${BACKUP_FILE}" && log "Backed up existing image ${ROOT_FILE} to ${BACKUP_FILE}"
 fi
 
 # Apply update
-mv "${UPDATE_FILE}" "${ROOT_FILE}" && echo "Applied updated rootfs from ${UPDATE_FILE}"
+mv "${UPDATE_FILE}" "${ROOT_FILE}" && log "Applied updated rootfs from ${UPDATE_FILE}"
 
 # Backup to a directory on the drive root file system
-mv "${OVERLAY_PATH}" "${BACKUP_PATH}" && echo "Backed up overlay ${OVERLAY_PATH} to ${BACKUP_PATH}"
+mv "${OVERLAY_PATH}" "${BACKUP_PATH}" && log "Backed up overlay ${OVERLAY_PATH} to ${BACKUP_PATH}"
 
 # Ensure paths exist for migrated data
 mkdir -p "${OVERLAY_PATH}/etc/NetworkManager"
@@ -54,34 +56,34 @@ mkdir -p "${OVERLAY_PATH}/opt/neon"
 mkdir -p "${OVERLAY_PATH}/var"
 
 # Migrate specific data back
-rm -rf "${BACKUP_PATH}/home/neon/venv" && echo "Removed old venv"
+rm -rf "${BACKUP_PATH}/home/neon/venv" && log "Removed old venv"
 
-mv "${BACKUP_PATH}/etc/NetworkManager/system-connections" "${OVERLAY_PATH}/etc/NetworkManager/" && echo "Restored Networks"
-mv "${BACKUP_PATH}/etc/ssh/"ssh_host_*_key* "${OVERLAY_PATH}/etc/ssh/" && echo "Restored SSH keys" || echo "No SSH keys to restore"
-[ -f "${BACKUP_PATH}/etc/shadow" ] && mv "${BACKUP_PATH}/etc/shadow" "${OVERLAY_PATH}/etc/" && echo "Restored User Passwords"
-[ -f "${BACKUP_PATH}/etc/hostname" ] && mv "${BACKUP_PATH}/etc/hostname" "${OVERLAY_PATH}/etc/" && echo "Restored Hostname"
-mv "${BACKUP_PATH}/etc/machine-id" "${OVERLAY_PATH}/etc/" && echo "Restored machine-id"
-mv "${BACKUP_PATH}/home" "${OVERLAY_PATH}/" && echo "Restored /home"
-mv "${BACKUP_PATH}/root" "${OVERLAY_PATH}/" && echo "Restored /root"
+mv "${BACKUP_PATH}/etc/NetworkManager/system-connections" "${OVERLAY_PATH}/etc/NetworkManager/" && log "Restored Networks"
+mv "${BACKUP_PATH}/etc/ssh/"ssh_host_*_key* "${OVERLAY_PATH}/etc/ssh/" && log "Restored SSH keys" || log "No SSH keys to restore"
+[ -f "${BACKUP_PATH}/etc/shadow" ] && mv "${BACKUP_PATH}/etc/shadow" "${OVERLAY_PATH}/etc/" && log "Restored User Passwords"
+[ -f "${BACKUP_PATH}/etc/hostname" ] && mv "${BACKUP_PATH}/etc/hostname" "${OVERLAY_PATH}/etc/" && log "Restored Hostname"
+mv "${BACKUP_PATH}/etc/machine-id" "${OVERLAY_PATH}/etc/" && log "Restored machine-id"
+mv "${BACKUP_PATH}/home" "${OVERLAY_PATH}/" && log "Restored /home"
+mv "${BACKUP_PATH}/root" "${OVERLAY_PATH}/" && log "Restored /root"
 
 # Restore signal files
-[ -f "${BACKUP_PATH}/opt/neon/firstboot" ] && mv "${BACKUP_PATH}/opt/neon/firstboot" "${OVERLAY_PATH}/opt/neon/" && echo "Restored firstboot flag"
-[ -f "${BACKUP_PATH}/opt/neon/do_sj201_config" ] && mv "${BACKUP_PATH}/opt/neon/do_sj201_config" "${OVERLAY_PATH}/opt/neon/" && echo "Restored do_sj201_config flag"
-[ -f "${BACKUP_PATH}/opt/neon/do_bluetooth_config" ] && mv "${BACKUP_PATH}/opt/neon/do_bluetooth_config" "${OVERLAY_PATH}/opt/neon/" && echo "Restored do_bluetooth_config flag"
+[ -f "${BACKUP_PATH}/opt/neon/firstboot" ] && mv "${BACKUP_PATH}/opt/neon/firstboot" "${OVERLAY_PATH}/opt/neon/" && log "Restored firstboot flag"
+[ -f "${BACKUP_PATH}/opt/neon/do_sj201_config" ] && mv "${BACKUP_PATH}/opt/neon/do_sj201_config" "${OVERLAY_PATH}/opt/neon/" && log "Restored do_sj201_config flag"
+[ -f "${BACKUP_PATH}/opt/neon/do_bluetooth_config" ] && mv "${BACKUP_PATH}/opt/neon/do_bluetooth_config" "${OVERLAY_PATH}/opt/neon/" && log "Restored do_bluetooth_config flag"
 
 # Restore specific `/var` paths (https://refspecs.linuxfoundation.org/FHS_3.0/fhs/ch05.html)
-[ -d "${BACKUP_PATH}/var/games" ] && mv "${BACKUP_PATH}/var/games" "${OVERLAY_PATH}/var/" && echo "Restored /var/games"
-[ -d "${BACKUP_PATH}/var/log" ] && mv "${BACKUP_PATH}/var/log" "${OVERLAY_PATH}/var/" && echo "Restored /var/log"
-[ -d "${BACKUP_PATH}/var/mail" ] && mv "${BACKUP_PATH}/var/mail" "${OVERLAY_PATH}/var/" && echo "Restored /var/mail"
+[ -d "${BACKUP_PATH}/var/games" ] && mv "${BACKUP_PATH}/var/games" "${OVERLAY_PATH}/var/" && log "Restored /var/games"
+[ -d "${BACKUP_PATH}/var/log" ] && mv "${BACKUP_PATH}/var/log" "${OVERLAY_PATH}/var/" && log "Restored /var/log"
+[ -d "${BACKUP_PATH}/var/mail" ] && mv "${BACKUP_PATH}/var/mail" "${OVERLAY_PATH}/var/" && log "Restored /var/mail"
 
 # Exclude crash information that relates to a previous OS
-#[ -d "${BACKUP_PATH}/var/crash" ] && mv "${BACKUP_PATH}/var/crash" "${OVERLAY_PATH}/var/" && echo "Restored /var/crash"
+#[ -d "${BACKUP_PATH}/var/crash" ] && mv "${BACKUP_PATH}/var/crash" "${OVERLAY_PATH}/var/" && log "Restored /var/crash"
 
 # Exclude package-specific data as the installed packages/versions may have changed
-#[ -d "${BACKUP_PATH}/var/lib" ] && mv "${BACKUP_PATH}/var/lib" "${OVERLAY_PATH}/var/" && echo "Restored /var/lib"
+#[ -d "${BACKUP_PATH}/var/lib" ] && mv "${BACKUP_PATH}/var/lib" "${OVERLAY_PATH}/var/" && log "Restored /var/lib"
 
 # Move any other data to a backup location
-mv "${BACKUP_PATH}" "${OVERLAY_PATH}/opt/neon/old_overlay" && echo "Archived old overlay"
+mv "${BACKUP_PATH}" "${OVERLAY_PATH}/opt/neon/old_overlay" && log "Archived old overlay"
 
 touch "${OVERLAY_PATH}/opt/neon/squashfs_updated"
-echo "Update complete"
+log "Update complete"
