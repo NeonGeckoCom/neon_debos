83d3be30a923a4d75ec731eb8af85ea8066517c2
NeonDaniel <NeonDaniel@users.noreply.github.com>
2023-09-01 22:07:19 +0000

    Increment Version to 23.9.1a22

--- version.py
+++ version.py
@@ -26,7 +26,7 @@
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE,  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-__version__ = "23.9.1a21"
+__version__ = "23.9.1a22"
 
 if __name__ == "__main__":
     print(__version__)
