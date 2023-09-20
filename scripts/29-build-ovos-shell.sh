7046997b85eb2c7e94260fda616004d372913602
Daniel McKnight <daniel@neon.ai>
2023-09-19 18:27:24 -0700

    Refactor shell build to use temporary directory

--- scripts/29-build-ovos-shell.sh
+++ scripts/29-build-ovos-shell.sh
@@ -27,7 +27,7 @@
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE,  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-BASE_DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BASE_DIR="/tmp"
 cd "${BASE_DIR}" || exit 10
 
 # Install embedded-shell
