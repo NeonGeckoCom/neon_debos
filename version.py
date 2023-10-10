8fe2467ebca2fb5752d566fb698486175295be4a
NeonDaniel <NeonDaniel@users.noreply.github.com>
2023-10-10 13:00:23 +0000

    Increment Version to 23.10.10a6

--- version.py
+++ version.py
@@ -26,7 +26,7 @@
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE,  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-__version__ = "23.10.6a5"
+__version__ = "23.10.10a6"
 
 if __name__ == "__main__":
     print(__version__)
