4b173c70a4f63e79b788808a071df247e3e2b85b
Daniel McKnight <daniel@neon.ai>
2024-02-13 13:39:13 -0800

    Update builds to support 6.x kernel

--- scripts/21-install_seeed_drivers.sh
+++ scripts/21-install_seeed_drivers.sh
@@ -49,11 +49,6 @@ dkms add -m $mod -v $ver
 
 echo "Looking for kernel with build dir in ${kernels[*]}"
 for current_kernel in "${kernels[@]}"; do
-  if [[ "${current_kernel}" == 6.* ]]; then
-    echo "Skipping Kernel 6.x build"
-  else
-    dkms build -k "${current_kernel}" -m $mod -v $ver && {
-            dkms install --force -k "${current_kernel}" -m $mod -v $ver
-            }
-  fi
+  dkms build -k "${current_kernel}" -m $mod -v $ver || continue
+  dkms install --force -k "${current_kernel}" -m $mod -v $ver
 done
