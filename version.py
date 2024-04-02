4b7d950aa9d58ed8564f5928c0fc6ffa259908df
NeonDaniel <NeonDaniel@users.noreply.github.com>
2024-04-02 17:24:15 +0000

    Increment Version to 24.4.2a27

--- version.py
+++ version.py
@@ -26,7 +26,7 @@
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE,  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-__version__ = "24.4.1a26"
+__version__ = "24.4.2a27"
 
 if __name__ == "__main__":
     print(__version__)
