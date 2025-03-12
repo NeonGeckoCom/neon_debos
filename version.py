d29d2a5aa33eeba6df2460000915a5a4d149d8ba
NeonDaniel <NeonDaniel@users.noreply.github.com>
2025-03-12 21:18:27 +0000

    Increment Version to 25.3.12a2

--- version.py
+++ version.py
@@ -26,7 +26,7 @@
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE,  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-__version__ = "24.9.20a1"
+__version__ = "25.3.12a2"
 
 if __name__ == "__main__":
     print(__version__)
