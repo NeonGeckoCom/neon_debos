e7c3178ed4d2cd4daa871c05488c459048c4355d
NeonDaniel <NeonDaniel@users.noreply.github.com>
2025-08-25 18:15:17 +0000

    Increment Version to 25.8.25

--- version.py
+++ version.py
@@ -26,7 +26,7 @@
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE,  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-__version__ = "25.3.17a5"
+__version__ = "25.8.25"
 
 if __name__ == "__main__":
     print(__version__)
